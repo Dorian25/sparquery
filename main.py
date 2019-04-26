fdcf1449b235cfd62361149723f1272143275da2
Dorian25 <hepar7@yahoo.fr>
2019-04-26 10:03:59 +0200

    main

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from ihm.MyApp import MyApp
-import subprocess
+#import subprocess
 
 if __name__ == "__main__":
     #main(sys.argv[1:])
-    
-    process = subprocess.Popen('cd stanford && java -mx4g -cp "*" edu.stanford.nlp.pipeline.StanfordCoreNLPServer',shell=True)
+    #pour lancer automatiquement le stanford server
+    #mais probleme au niveau du processus qui reste toujours actif
+    #meme apres la fermeture de l'application
+    #process = subprocess.Popen('cd stanford && java -mx4g -cp "*" edu.stanford.nlp.pipeline.StanfordCoreNLPServer',shell=True)
     MyApp().mainloop()
-    process.kill()
-    print("process killed")
+    #process.kill()
+    #print("process killed")
